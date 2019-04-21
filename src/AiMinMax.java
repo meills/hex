4a2b8534bf7795be280514aef4bca4364e015bd9
Amelia Oh <amelia.ohml@gmail.com>
2019-04-21 14:43:16 +0100

    starting AiMinMax implementation

---
+++ src/AiMinMax.java
@@ -0,0 +1,43 @@
+import java.util.HashSet;
+import java.util.Random;
+
+/**
+ * This class is the AI that uses min max & alpha beta pruning.
+ */
+public class AiMinMax {
+    private double alpha;
+    private double beta;
+
+    public static HashSet<int[]> line;
+
+    /**
+     * Main method created here to allow for debugging of individual class
+     * can be deleted later
+     */
+    public static void main(String[] args) {
+
+    }
+
+    public static void setLine() {
+        Random rand = new Random();
+        int[] coords;
+        int row;
+        line = new HashSet<>();
+        row = rand.nextInt(Board.BOARD_SIZE);
+
+        //sets coords for edge piece to generate jagged line from
+        row
+
+        /**
+         * Generates a random line.
+         */
+        for (int i = 0; i < Board.BOARD_SIZE; i++) {
+            coords = new int[2];
+            coords[0] = row;
+            coords[1] = i;
+            line.add(coords);
+        }
+    }
+
+
+}
---
+++ src/Heuristics.java
@@ -0,0 +1,25 @@
+/**
+ * Heuristics used to calculate score for min max.
+ */
+public class Heuristics {
+    private static final int N_COEFF = 5;
+
+    /**
+     * This heuristic looks at how many choices the option has.
+     * The more "neighbours"it has the better.
+     */
+    private double choices;
+
+    /**
+     * Heuristic that looks at
+     */
+    private double proximity;
+
+
+    public static void calcChoices {
+
+
+    }
+
+
+}
